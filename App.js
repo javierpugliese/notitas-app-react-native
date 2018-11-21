aae01b9a27ce03ab6296e424319e58bc1d6066c1
javierpugliese <javierpugliese2000@gmail.com>
2018-11-21 16:37:02 -0300

    Scroll, input y boton para las notas

--- App.js
+++ App.js
@@ -11,10 +11,26 @@ import {
 export default class App extends React.Component {
   render() {
     return (
-      <View style={estilos.contenedor}>
+      <View style={styles.container}>
 
-        <View style={estilos.encabezado}>
-          <Text style={estilos.textoEncabezado}>- NOTITAS -</Text>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>- NOTITAS -</Text>
+        </View>
+
+        <ScrollView style={styles.scrollContainer}></ScrollView>
+
+        <View style={styles.footer}>
+          <TouchableOpacity style={styles.addButton}>
+            <Text style={styles.addButtonText}>+</Text>
+          </TouchableOpacity>
+
+          <TextInput
+            style={styles.textInput}
+            placeholder='>>> Escribir notita'
+            placeholderTextColor='white'
+            underlineColorAndroid='transparent'>
+
+          </TextInput>
         </View>
 
       </View>
@@ -22,20 +38,57 @@ export default class App extends React.Component {
   }
 }
 
-const estilos = StyleSheet.create({
-  contenedor: {
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
   },
-  encabezado: {
-    backgroundColor: 'green',
+  header: {
+    backgroundColor: '#774c2b',
     alignItems: 'center',
     justifyContent: 'center',
     borderBottomWidth: 10,
     borderBottomColor: '#ddd',
   },
-  textoEncabezado: {
+  headerText: {
     color: 'white',
     fontSize: 18,
     padding: 26,
+  },
+  scrollContainer: {
+    flex: 1,
+    marginBottom: 100,
+  },
+  footer: {
+    position: 'absolute',
+    alignItems: 'center',
+    bottom: 0,
+    left: 0,
+    right: 0.
+  },
+  addButton: {
+    backgroundColor: '#774c2b',
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+    borderColor: '#ccc',
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 8,
+    marginBottom: -45,
+    zIndex: 10,
+  },
+  addButtonText: {
+    color: '#fff',
+    fontSize: 24,
+  },
+  textInput: {
+    alignSelf: 'stretch',
+    color: '#fff',
+    padding: 20,
+    paddingTop: 46,
+    backgroundColor: '#d86e2d',
+    borderTopWidth: 2,
+    borderTopColor: '#ededed',
+
   },
 });
